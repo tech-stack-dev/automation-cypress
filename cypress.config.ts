0c4d9187be9fa4ca32f759139a5f9e95a16ad776
Kostiantyn Rybak <kostiantyn.rybak@tech-stack.io>
2023-10-03 15:58:46 +0300

    Fixing lint errors, additional config options

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress';
+import fs from 'fs';
 
 const requirements = require('./environments.json');
 
@@ -6,9 +7,22 @@ export default defineConfig({
   env: {
     baseUrl: requirements.Staging
   },
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  video: true,
+  videoCompression: 16,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
+      on('after:spec', (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+        if (results?.video) {
+          const failures = results.tests.some((test) => test.attempts.some((attempt) => attempt.state === 'failed'));
+          if (!failures) {
+            fs.unlinkSync(results.video);
+          }
+        }
+      });
+      return config;
+    },
+    specPattern: 'cypress/e2e/**/*.cy.ts'
   }
 });
