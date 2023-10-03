c0ba0bd56512d3dba613e70fb59337242c42d48b
Kostiantyn Rybak <kostiantyn.rybak@tech-stack.io>
2023-10-03 14:51:27 +0300

    Adding environments config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "cypress";
 
+const requirements = require('./environments.json');
+
 export default defineConfig({
+  env: {
+    baseUrl: requirements.Staging
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
