4c783a8d2edf426e3db12d6b97751917a9a5b9bd
Kostiantyn Rybak <kostiantyn.rybak@tech-stack.io>
2023-11-28 18:56:13 +0200

    Adding new test with mocked data

--- cypress/e2e/ui/ui.test.spec.cy.ts
+++ cypress/e2e/ui/ui.test.spec.cy.ts
@@ -1,5 +1,6 @@
 import CommonActions from '../../helpers/commonActions';
 import AiDevelopmentPage from '../../pages/aiDevelopmentPage';
+import HealthcarePage from '../../pages/healthcarePage';
 import TopMenu from '../../pages/topMenu';
 
 describe('Top menu tests', () => {
@@ -19,8 +20,23 @@ describe('Top menu tests', () => {
     });
   });
 
-  it('Menu navigation test', () => {
+  it('Menu navigation test: Services -> AI Development', () => {
     TopMenu.navigate(TopMenu.tabs.SERVICES, 'AI Development');
     AiDevelopmentPage.verifyIsOpened();
+  });
+
+  it('Using mocked data when loading the page', () => {
+    const url = '/blog/ghost/api/v3/content/posts/**';
+    const mockDataFile1 = 'healthcareMockData1.json';
+    const mockDataFile2 = 'healthcareMockData2.json';
+
+    TopMenu.navigate(TopMenu.tabs.INDUSTRIES, 'Healthcare');
+    HealthcarePage.verifyIsOpened();
+    // Example of working with fixtures
+    cy.fixture(mockDataFile1).then((mockData) => HealthcarePage.refreshAndMockData('GET', url, { body: mockData }));
+    HealthcarePage.checkRelatedArticle('The Test Post 1');
+    // One more example of working with fixtures when mocking response
+    HealthcarePage.refreshAndMockData('GET', url, { fixture: mockDataFile2 });
+    HealthcarePage.checkRelatedArticle('The Test Post 2');
   });
 });
